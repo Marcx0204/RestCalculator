fca810c01ee6a385279d73f633e7fc060aa09406
Marc Simoneit <wi21b112@technikum-wien.at>
2022-06-02 16:34:19 +0200

    Add calculator functions

--- src/main/java/com/example/restcalculator/CalculatorController.java
+++ src/main/java/com/example/restcalculator/CalculatorController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+// str k zum commiten
+// str shift k zum pushen
+// http://localhost:8080/sum?a=817234&b=19824
 @RestController
 public class CalculatorController {
 
@@ -14,5 +17,37 @@ public class CalculatorController {
     ) {
         return a+b;
     }
+    @RequestMapping("/min")
+    public int min (
+            @RequestParam int a,
+            @RequestParam int b
+    ) {
+        return a-b;
+    }
 
+    @RequestMapping("/mul")
+    public int mul (
+            @RequestParam int a,
+            @RequestParam int b
+    ) {
+        return a*b;
+    }
+    @RequestMapping("/div")
+    public int div (
+            @RequestParam int a,
+            @RequestParam int b
+    ) {
+        return a/b;
+    }
+    @RequestMapping("/expo")
+    public int expo (
+            @RequestParam int a,
+            @RequestParam int b
+    ) {
+        int erg= a;
+        for (int i = 1; i < b;i++) {
+            erg = erg * a;
+        }
+        return erg;
+    }
 }
